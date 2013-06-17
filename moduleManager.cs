46b2a7b0569a56a100a22de6e3b94d34d02e76de
Administrator <Brent@Brents-MacBook-Pro.local>
2013-06-17 01:37:23 -0700

    ModuleManager v1.4

--- moduleManager.cs
+++ moduleManager.cs
@@ -1,49 +1,12 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
-
 using UnityEngine;
-
 using KSP;
-using KSP.IO;
 
-/* Ideal use cases
- * Part.Load(ConfigNode)
- * {
- *   this will set the Fields, 
- *   then load all attachNodes,
- *   then load all Resources,
- *   then load all Modules.
- * 
- *   It will work whether ConfigNode is a straight PART {} node, or a @PART[name] node.
- * }
- * 
- * AvailablePart.Load(ConfigNode)
- * {
- *   this will call Part.Load(ConfigNode)
- *   then reset resourceInfo
- *   then reset moduleInfo 
- * }
- * 
- * List<ConfigNode> GameDatabase
- * this will be every possible top-level ConfigNode pulled from every .cfg file in the GameData folder,
- * concatenated as subnodes of one single massive ConfigNode
- * This ConfigNode will also have values for every game asset
- * such as mesh =
- * and texture =
- * and sound =
- * and so on.
- * 
- * */
 namespace ModuleManager
 {
 	[KSPAddon(KSPAddon.Startup.Instantly, true)]
 	public class ConfigManager : MonoBehaviour
 	{
-
-		public static List<AvailablePart> partDatabase = PartLoader.LoadedPartsList;
-
 		//FindConfigNodeIn finds and returns a ConfigNode in src of type nodeType. 
 		//If nodeName is not null, it will only find a node of type nodeType with the value name=nodeName. 
 		//If nodeTag is not null, it will only find a node of type nodeType with the value name=nodeName and tag=nodeTag.
@@ -69,6 +32,7 @@ namespace ModuleManager
 			}
 			return null;
 		}
+
 
 		//ModifyNode applies the ConfigNode mod as a 'patch' to ConfigNode original, then returns the patched ConfigNode.
 		// it uses FindConfigNodeIn(src, nodeType, nodeName, nodeTag) to recurse.
@@ -160,39 +124,39 @@ namespace ModuleManager
 			return newNode;
 		}
 
-		public static List<ConfigNode> AllConfigsStartingWith(string match) {
-			List<ConfigNode> nodes = new List<ConfigNode>();
+		public static List<UrlDir.UrlConfig> AllConfigsStartingWith(string match) {
+			List<UrlDir.UrlConfig> nodes = new List<UrlDir.UrlConfig>();
 			foreach (UrlDir.UrlConfig url in GameDatabase.Instance.root.AllConfigs) {
 				if(url.type.StartsWith (match))
 					url.config.name = url.type;
-					nodes.Add (url.config);
+					nodes.Add (url);
 			}
 			return nodes;
 		}
 
+		bool loaded = false;
 		public void OnGUI()
 		{
+			if (loaded)
+				return;
 			//by the time we reach OnGUI(), all the configNodes have been loaded.
 			print ("ModuleManager loading cfg patches...");
 			foreach (UrlDir.UrlConfig url in GameDatabase.Instance.root.AllConfigs) {
 				if (url.type [0] != '@') {
 					string modName = "@" + url.type + "[" + url.name + "]";
-					foreach (ConfigNode mod in GameDatabase.Instance.GetConfigNodes(modName)) {
-						print ("Applying node " + modName);
-						url.config = ModifyNode (url.config, mod);
+					foreach (UrlDir.UrlConfig mod in GameDatabase.Instance.GetConfigs(modName)) {
+						print ("Applying node " + mod.url);
+						url.config = ModifyNode (url.config, mod.config);
 					}
-				
+
 					modName = "@" + url.type + "[" + url.name + "]:Final";
-					foreach (ConfigNode mod in GameDatabase.Instance.GetConfigNodes(modName)) {
-						print ("Applying node " + modName);
-						url.config = ModifyNode (url.config, mod);
-					}										
+					foreach (UrlDir.UrlConfig mod in GameDatabase.Instance.GetConfigs(modName)) {
+						print ("Applying node " + mod.url);
+						url.config = ModifyNode (url.config, mod.config);
+					}
 				}
 			}
-
-			// we're done; no need to stick around.
-			DestroyImmediate (this);
-				
+			loaded = true;
 		}
 	}
 }
