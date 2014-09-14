0aa5b8b674d8c32769bfe667fa18d0e267cab586
Sarbian <Sarbian@sarbian.com>
2014-09-14 22:38:54 +0200

    Add missing using

--- moduleManager.cs
+++ moduleManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Text.RegularExpressions;
 using KSP;
 using UnityEngine;
