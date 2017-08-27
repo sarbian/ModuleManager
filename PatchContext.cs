abba87ea8b94ea6cab2ab2a25828f7aed756fc8e
blowfish <joeydwong@gmail.com>
2017-08-26 20:06:55 -0700

    Add PatchContext struct

---
+++ PatchContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModuleManager
+{
+    public struct PatchContext
+    {
+        public readonly UrlDir.UrlConfig patchUrl;
+        public readonly UrlDir databaseRoot;
+
+        public PatchContext(UrlDir.UrlConfig patchUrl, UrlDir databaseRoot)
+        {
+            this.patchUrl = patchUrl;
+            this.databaseRoot = databaseRoot;
+        }
+    }
+}
