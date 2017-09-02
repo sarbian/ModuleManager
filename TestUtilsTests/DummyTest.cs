a7f901e4654900cde28f93c15e27154d66a36e30
blowfish <joeydwong@gmail.com>
2017-09-01 22:16:07 -0700

    Yo dawg, I heard you like tests

---
+++ TestUtilsTests/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace TestUtilsTests
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
