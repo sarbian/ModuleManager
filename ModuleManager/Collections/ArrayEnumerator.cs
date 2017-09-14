24f21435b3efc6e0064de654a679ec8d5afba2d3
blowfish <joeydwong@gmail.com>
2017-09-13 21:10:48 -0700

    Add ArrayEnumerator
    Enumerates arrays in a garbage-free way

---
+++ ModuleManager/Collections/ArrayEnumerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ModuleManager.Collections
+{
+    public struct ArrayEnumerator<T> : IEnumerator<T>
+    {
+        private readonly T[] array;
+        private int index;
+
+        public ArrayEnumerator(T[] array)
+        {
+            this.array = array;
+            index = -1;
+        }
+
+        public T Current => array[index];
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            index++;
+            return index < array.Length;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+}
