7d0c5878014b0e000808f8cb7b04d5665e280aa9
blowfish <joeydwong@gmail.com>
2017-09-19 23:26:45 -0700

    Add QueueLogger and supporting classes
    Allows logging to a queue

---
+++ ModuleManager/Logging/ExceptionMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ModuleManager.Logging
+{
+    public class ExceptionMessage : ILogMessage
+    {
+        public readonly string message;
+        public readonly Exception exception;
+
+        public ExceptionMessage(string message, Exception exception)
+        {
+            this.message = message;
+            this.exception = exception;
+        }
+
+        public void LogTo(IBasicLogger logger)
+        {
+            logger.Exception(message, exception);
+        }
+    }
+}
---
+++ ModuleManagerTests/Logging/NormalMessageTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using NSubstitute;
+using UnityEngine;
+using ModuleManager.Logging;
+
+namespace ModuleManagerTests.Logging
+{
+    public class NormalMessageTest
+    {
+        private IBasicLogger logger = Substitute.For<IBasicLogger>();
+
+        [Fact]
+        public void TestLogTo__Info()
+        {
+            NormalMessage message = new NormalMessage(LogType.Log, "everything is ok");
+            message.LogTo(logger);
+            logger.Received().Log(LogType.Log, "everything is ok");
+        }
+
+        [Fact]
+        public void TestLogTo__Warning()
+        {
+            NormalMessage message = new NormalMessage(LogType.Warning, "I'm warning you");
+            message.LogTo(logger);
+            logger.Received().Log(LogType.Warning, "I'm warning you");
+        }
+
+        [Fact]
+        public void TestLogTo__Error()
+        {
+            NormalMessage message = new NormalMessage(LogType.Error, "You went too far");
+            message.LogTo(logger);
+            logger.Received().Log(LogType.Error, "You went too far");
+        }
+    }
+}
