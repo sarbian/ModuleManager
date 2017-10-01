2dc9f9121be35ea1c30ada09c0dc0456cc7b0c1a
blowfish <joeydwong@gmail.com>
2017-10-01 14:14:55 -0700

    Extract FileSHA
    Interacts with the file system so difficult to test unfortunately

--- ModuleManager/MMPatchLoader.cs
+++ ModuleManager/MMPatchLoader.cs
@@ -13,6 +13,7 @@ using Debug = UnityEngine.Debug;
 
 using ModuleManager.Logging;
 using ModuleManager.Extensions;
+using ModuleManager.Utils;
 using NodeStack = ModuleManager.Collections.ImmutableStack<ConfigNode>;
 
 namespace ModuleManager
@@ -177,7 +178,7 @@ namespace ModuleManager
                 string fileSha;
                 try
                 {
-                    fileSha = FileSHA(mod.assembly.Location);
+                    fileSha = FileUtils.FileSHA(mod.assembly.Location);
                 }
                 catch(Exception e)
                 {
@@ -499,28 +500,6 @@ namespace ModuleManager
             }
 
             configs[0].config.Save(physicsPath);
-        }
-
-        private static string FileSHA(string filename)
-        {
-            if (!File.Exists(filename)) throw new FileNotFoundException("File does not exist", filename);
-
-            System.Security.Cryptography.SHA256 sha = System.Security.Cryptography.SHA256.Create();
-
-            byte[] data = null;
-            using (FileStream fs = File.Open(filename, FileMode.Open, FileAccess.Read))
-            {
-                data = sha.ComputeHash(fs);
-            }
-
-            string hashedValue = string.Empty;
-
-            foreach (byte b in data)
-            {
-                hashedValue += String.Format("{0,2:x2}", b);
-            }
-
-            return hashedValue;
         }
 
         private void IsCacheUpToDate()
