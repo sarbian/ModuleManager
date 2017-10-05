172e2f3c845c42f1a4b4fb14182314891b00939a
blowfish <joeydwong@gmail.com>
2017-10-04 22:38:41 -0700

    Add background task support
    Allows a background task to be run and monitored, including if it exits
    due to an exception

---
+++ ModuleManager/Threading/BackgroundTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ModuleManager.Threading
+{
+    public static class BackgroundTask
+    {
+        public static ITaskStatus Start(Action action)
+        {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+
+            TaskStatus status = new TaskStatus();
+
+            void RunAction()
+            {
+                try
+                {
+                    action();
+                    status.Finished();
+                }
+                catch (Exception ex)
+                {
+                    status.Error(ex);
+                }
+            }
+
+            Thread thread = new Thread(RunAction);
+            thread.Start();
+
+            return new TaskStatusWrapper(status);
+        }
+    }
+}
