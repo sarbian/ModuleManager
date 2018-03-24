e03db37b1605fe8496b3456f0af5a7a752439442
blowfish <joeydwong@gmail.com>
2018-03-24 12:09:39 -0700

    implement stream logger
    log to a stream

---
+++ ModuleManager/Logging/StreamLogger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace ModuleManager.Logging
+{
+    public class StreamLogger : IBasicLogger, IDisposable
+    {
+        private readonly Stream stream;
+        private readonly StreamWriter streamWriter;
+        private readonly IBasicLogger exceptionLogger;
+        private bool disposed = false;
+
+        public StreamLogger(Stream stream, IBasicLogger exceptionLogger)
+        {
+            this.stream = stream ?? throw new ArgumentNullException(nameof(stream));
+            if (!stream.CanWrite) throw new ArgumentException("must be writable", nameof(stream));
+            streamWriter = new StreamWriter(stream);
+            this.exceptionLogger = exceptionLogger ?? throw new ArgumentNullException(nameof(exceptionLogger));
+        }
+
+        public void Log(LogType logType, string message)
+        {
+            if (disposed) throw new InvalidOperationException("Object has already been disposed");
+            try
+            {
+                string prefix;
+                if (logType == LogType.Log)
+                    prefix = "LOG";
+                else if (logType == LogType.Warning)
+                    prefix = "WRN";
+                else if (logType == LogType.Error)
+                    prefix = "ERR";
+                else if (logType == LogType.Assert)
+                    prefix = "AST";
+                else if (logType == LogType.Exception)
+                    prefix = "EXC";
+                else
+                    prefix = "UNK";
+
+                streamWriter.WriteLine("[{0} {1}] {2}", prefix, DateTime.Now.ToString(), message);
+            }
+            catch (Exception e)
+            {
+                exceptionLogger.Exception("Exception while attempting to log to stream", e);
+            }
+        }
+
+        public void Exception(string message, Exception exception)
+        {
+            Log(LogType.Exception, exception?.ToString() ?? "<null exception>");
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                // Flushes and closes the StreamWriter and the underlying stream
+                streamWriter.Close();
+            }
+            catch(Exception e)
+            {
+                exceptionLogger.Exception("Exception while attempting to close stream writer", e);
+            }
+
+            disposed = true;
+        }
+    }
+}
---
+++ ModuleManagerTests/Logging/StreamLoggerTest.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using Xunit;
+using NSubstitute;
+using ModuleManager.Logging;
+
+namespace ModuleManagerTests.Logging
+{
+    public class StreamLoggerTest
+    {
+        [Fact]
+        public void TestConstructor__StreamNull()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                new StreamLogger(null, Substitute.For<IBasicLogger>());
+            });
+
+            Assert.Equal("stream", ex.ParamName);
+        }
+
+        [Fact]
+        public void TestConstructor__CantWrite()
+        {
+            using (MemoryStream stream = new MemoryStream(new byte[0], false))
+            {
+                ArgumentException ex = Assert.Throws<ArgumentException>(delegate
+                {
+                    new StreamLogger(stream, Substitute.For<IBasicLogger>());
+                });
+
+                Assert.Equal("stream", ex.ParamName);
+                Assert.Contains("must be writable", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void TestConstructor__ExceptionLoggerNull()
+        {
+            using (MemoryStream stream = new MemoryStream(new byte[0], true))
+            {
+                ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+                {
+                    new StreamLogger(stream, null);
+                });
+
+                Assert.Equal("exceptionLogger", ex.ParamName);
+            }
+        }
+
+        [Fact]
+        public void TestLog__AlreadyDisposed()
+        {
+            using (MemoryStream stream = new MemoryStream(new byte[0], true))
+            {
+                StreamLogger streamLogger = new StreamLogger(stream, Substitute.For<IBasicLogger>());
+                streamLogger.Dispose();
+
+                InvalidOperationException ex = Assert.Throws<InvalidOperationException>(delegate
+                {
+                    streamLogger.Log(LogType.Log, "a message");
+                });
+
+                Assert.Contains("Object has already been disposed", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void TestLog__Log()
+        {
+            IBasicLogger exceptionLogger = Substitute.For<IBasicLogger>();
+            byte[] bytes = new byte[50];
+            using (MemoryStream stream = new MemoryStream(bytes, true))
+            {
+                using (StreamLogger streamLogger = new StreamLogger(stream, exceptionLogger))
+                {
+                    streamLogger.Log(LogType.Log, "a message");
+                }
+            }
+
+            exceptionLogger.DidNotReceiveWithAnyArgs().Exception(null, null);
+
+            using (MemoryStream stream = new MemoryStream(bytes, false))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    Assert.Contains("[LOG ", result);
+                    Assert.Contains("] a message", result);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestLog__Assert()
+        {
+            IBasicLogger exceptionLogger = Substitute.For<IBasicLogger>();
+            byte[] bytes = new byte[50];
+            using (MemoryStream stream = new MemoryStream(bytes, true))
+            {
+                using (StreamLogger streamLogger = new StreamLogger(stream, exceptionLogger))
+                {
+                    streamLogger.Log(LogType.Assert, "a message");
+                }
+            }
+
+            exceptionLogger.DidNotReceiveWithAnyArgs().Exception(null, null);
+
+            using (MemoryStream stream = new MemoryStream(bytes, false))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    Assert.Contains("[AST ", result);
+                    Assert.Contains("] a message", result);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestLog__Warning()
+        {
+            IBasicLogger exceptionLogger = Substitute.For<IBasicLogger>();
+            byte[] bytes = new byte[50];
+            using (MemoryStream stream = new MemoryStream(bytes, true))
+            {
+                using (StreamLogger streamLogger = new StreamLogger(stream, exceptionLogger))
+                {
+                    streamLogger.Log(LogType.Warning, "a message");
+                }
+            }
+
+            exceptionLogger.DidNotReceiveWithAnyArgs().Exception(null, null);
+
+            using (MemoryStream stream = new MemoryStream(bytes, false))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    Assert.Contains("[WRN ", result);
+                    Assert.Contains("] a message", result);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestLog__Error()
+        {
+            IBasicLogger exceptionLogger = Substitute.For<IBasicLogger>();
+            byte[] bytes = new byte[50];
+            using (MemoryStream stream = new MemoryStream(bytes, true))
+            {
+                using (StreamLogger streamLogger = new StreamLogger(stream, exceptionLogger))
+                {
+                    streamLogger.Log(LogType.Error, "a message");
+                }
+            }
+
+            exceptionLogger.DidNotReceiveWithAnyArgs().Exception(null, null);
+
+            using (MemoryStream stream = new MemoryStream(bytes, false))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    Assert.Contains("[ERR ", result);
+                    Assert.Contains("] a message", result);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestLog__Exception()
+        {
+            IBasicLogger exceptionLogger = Substitute.For<IBasicLogger>();
+            byte[] bytes = new byte[50];
+            using (MemoryStream stream = new MemoryStream(bytes, true))
+            {
+                using (StreamLogger streamLogger = new StreamLogger(stream, exceptionLogger))
+                {
+                    streamLogger.Log(LogType.Exception, "a message");
+                }
+            }
+
+            exceptionLogger.DidNotReceiveWithAnyArgs().Exception(null, null);
+
+            using (MemoryStream stream = new MemoryStream(bytes, false))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    Assert.Contains("[EXC ", result);
+                    Assert.Contains("] a message", result);
+                }
+            }
+        }
+
+        [Fact]
+        public void TestLog__Unknown()
+        {
+            IBasicLogger exceptionLogger = Substitute.For<IBasicLogger>();
+            byte[] bytes = new byte[50];
+            using (MemoryStream stream = new MemoryStream(bytes, true))
+            {
+                using (StreamLogger streamLogger = new StreamLogger(stream, exceptionLogger))
+                {
+                    streamLogger.Log((LogType)1000, "a message");
+                }
+            }
+
+            exceptionLogger.DidNotReceiveWithAnyArgs().Exception(null, null);
+
+            using (MemoryStream stream = new MemoryStream(bytes, false))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    Assert.Contains("[UNK ", result);
+                    Assert.Contains("] a message", result);
+                }
+            }
+        }
+    }
+}
