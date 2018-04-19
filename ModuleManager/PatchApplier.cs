53d238a7ccc585c66ef21536fa5dd62fbea70328
blowfish <joeydwong@gmail.com>
2018-04-19 00:07:26 -0700

    Extract IPatch interface, split up root patches

--- ModuleManager/PatchApplier.cs
+++ ModuleManager/PatchApplier.cs
@@ -2,8 +2,8 @@
 using System.Linq;
 using ModuleManager.Logging;
 using ModuleManager.Extensions;
+using ModuleManager.Patches;
 using ModuleManager.Progress;
-using NodeStack = ModuleManager.Collections.ImmutableStack<ConfigNode>;
 
 namespace ModuleManager
 {
@@ -11,8 +11,7 @@ namespace ModuleManager
     {
         private readonly IBasicLogger logger;
         private readonly IPatchProgress progress;
-
-        private readonly UrlDir databaseRoot;
+        
         private readonly IPatchList patchList;
 
         private readonly UrlDir.UrlFile[] allConfigFiles;
@@ -22,7 +21,6 @@ namespace ModuleManager
         public PatchApplier(IPatchList patchList, UrlDir databaseRoot, IPatchProgress progress, IBasicLogger logger)
         {
             this.patchList = patchList;
-            this.databaseRoot = databaseRoot;
             this.progress = progress;
             this.logger = logger;
 
@@ -42,112 +40,23 @@ namespace ModuleManager
             logger.Info(pass.Name + " pass");
             Activity = "ModuleManager " + pass.Name;
 
-            foreach (Patch patch in pass)
+            foreach (IPatch patch in pass)
             {
                 try
                 {
-                    string name = patch.node.name.RemoveWS();
-
-                    if (patch.command == Command.Insert)
-                    {
-                        logger.Warning("Warning - Encountered insert node that should not exist at this stage: " + patch.urlConfig.SafeUrl());
-                        continue;
-                    }
-                    else if (patch.command != Command.Edit && patch.command != Command.Copy && patch.command != Command.Delete)
-                    {
-                        logger.Warning("Invalid command encountered on a patch: " + patch.urlConfig.SafeUrl());
-                        continue;
-                    }
-
-                    string upperName = name.ToUpper();
-                    PatchContext context = new PatchContext(patch.urlConfig, databaseRoot, logger, progress);
-                    char[] sep = { '[', ']' };
-                    string condition = "";
-
-                    if (upperName.Contains(":HAS["))
-                    {
-                        int start = upperName.IndexOf(":HAS[");
-                        condition = name.Substring(start + 5, name.LastIndexOf(']') - start - 5);
-                        name = name.Substring(0, start);
-                    }
-
-                    string[] splits = name.Split(sep, 3);
-                    string[] patterns = splits.Length > 1 ? splits[1].Split(',', '|') : null;
-                    string type = splits[0];
-
-                    bool loop = patch.node.HasNode("MM_PATCH_LOOP");
-
                     foreach (UrlDir.UrlFile file in allConfigFiles)
                     {
-                        if (patch.command == Command.Edit)
-                        {
-                            foreach (UrlDir.UrlConfig url in file.configs)
-                            {
-                                if (!patch.nodeMatcher.IsMatch(url.config)) continue;
-                                if (loop) logger.Info($"Looping on {patch.urlConfig.SafeUrl()} to {url.SafeUrl()}");
-
-                                do
-                                {
-                                    progress.ApplyingUpdate(url, patch.urlConfig);
-                                    url.config = MMPatchLoader.ModifyNode(new NodeStack(url.config), patch.node, context);
-                                } while (loop && patch.nodeMatcher.IsMatch(url.config));
-
-                                if (loop) url.config.RemoveNodes("MM_PATCH_LOOP");
-                            }
-                        }
-                        else if (patch.command == Command.Copy)
-                        {
-                            // Avoid checking the new configs we are creating
-                            int count = file.configs.Count;
-                            for (int i = 0; i < count; i++)
-                            {
-                                UrlDir.UrlConfig url = file.configs[i];
-                                if (!patch.nodeMatcher.IsMatch(url.config)) continue;
-
-                                ConfigNode clone = MMPatchLoader.ModifyNode(new NodeStack(url.config), patch.node, context);
-                                if (url.config.HasValue("name") && url.config.GetValue("name") == clone.GetValue("name"))
-                                {
-                                    progress.Error(patch.urlConfig, $"Error - when applying copy {patch.urlConfig.SafeUrl()} to {url.SafeUrl()} - the copy needs to have a different name than the parent (use @name = xxx)");
-                                }
-                                else
-                                {
-                                    progress.ApplyingCopy(url, patch.urlConfig);
-                                    file.AddConfig(clone);
-                                }
-                            }
-                        }
-                        else if (patch.command == Command.Delete)
-                        {
-                            int i = 0;
-                            while (i < file.configs.Count)
-                            {
-                                UrlDir.UrlConfig url = file.configs[i];
-
-                                if (patch.nodeMatcher.IsMatch(url.config))
-                                {
-                                    progress.ApplyingDelete(url, patch.urlConfig);
-                                    file.configs.RemoveAt(i);
-                                }
-                                else
-                                {
-                                    i++;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            throw new NotImplementedException("This code should not be reachable");
-                        }
+                        patch.Apply(file, progress, logger);
                     }
                     progress.PatchApplied();
                 }
                 catch (Exception e)
                 {
-                    progress.Exception(patch.urlConfig, "Exception while processing node : " + patch.urlConfig.SafeUrl(), e);
+                    progress.Exception(patch.UrlConfig, "Exception while processing node : " + patch.UrlConfig.SafeUrl(), e);
 
                     try
                     {
-                        logger.Error("Processed node was\n" + patch.urlConfig.PrettyPrint());
+                        logger.Error("Processed node was\n" + patch.UrlConfig.PrettyPrint());
                     }
                     catch (Exception ex2)
                     {
---
+++ ModuleManagerTests/Patches/CopyPatchTest.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using Xunit;
+using NSubstitute;
+using TestUtils;
+using ModuleManager;
+using ModuleManager.Logging;
+using ModuleManager.Patches;
+using ModuleManager.Progress;
+
+namespace ModuleManagerTests.Patches
+{
+    public class CopyPatchTest
+    {
+        [Fact]
+        public void TestConstructor__urlConfigNull()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                new CopyPatch(null, Substitute.For<INodeMatcher>());
+            });
+
+            Assert.Equal("urlConfig", ex.ParamName);
+        }
+
+        [Fact]
+        public void TestConstructor__nodeMatcherNull()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                new CopyPatch(UrlBuilder.CreateConfig("abc/def", new ConfigNode()), null);
+            });
+
+            Assert.Equal("nodeMatcher", ex.ParamName);
+        }
+
+        [Fact]
+        public void TestUrlConfig()
+        {
+            UrlDir.UrlConfig urlConfig = UrlBuilder.CreateConfig("abc/def", new ConfigNode());
+            CopyPatch patch = new CopyPatch(urlConfig, Substitute.For<INodeMatcher>());
+
+            Assert.Same(urlConfig, patch.UrlConfig);
+        }
+
+        [Fact]
+        public void TestNodeMatcher()
+        {
+            INodeMatcher nodeMatcher = Substitute.For<INodeMatcher>();
+            CopyPatch patch = new CopyPatch(UrlBuilder.CreateConfig("abc/def", new ConfigNode()), nodeMatcher);
+
+            Assert.Same(nodeMatcher, patch.NodeMatcher);
+        }
+
+        [Fact]
+        public void TestApply()
+        {
+            UrlDir.UrlFile file = UrlBuilder.CreateFile("abc/def.cfg");
+
+            UrlDir.UrlConfig urlConfig1 = UrlBuilder.CreateConfig(new TestConfigNode("NODE")
+            {
+                { "foo", "bar" },
+            }, file);
+
+            UrlDir.UrlConfig urlConfig2 = UrlBuilder.CreateConfig(new TestConfigNode("NODE")
+            {
+                { "foo", "bar" },
+            }, file);
+
+            UrlDir.UrlConfig urlConfig3 = UrlBuilder.CreateConfig(new ConfigNode("NODE"), file);
+            UrlDir.UrlConfig urlConfig4 = UrlBuilder.CreateConfig(new ConfigNode("NODE"), file);
+
+            INodeMatcher nodeMatcher = Substitute.For<INodeMatcher>();
+
+            nodeMatcher.IsMatch(urlConfig1.config).Returns(false);
+            nodeMatcher.IsMatch(urlConfig2.config).Returns(true);
+            nodeMatcher.IsMatch(urlConfig3.config).Returns(false);
+            nodeMatcher.IsMatch(urlConfig4.config).Returns(true);
+
+            CopyPatch patch = new CopyPatch(UrlBuilder.CreateConfig("ghi/jkl", new TestConfigNode("@NODE")
+            {
+                { "@foo", "baz" },
+                { "pqr", "stw" },
+            }), nodeMatcher);
+            
+            IPatchProgress progress = Substitute.For<IPatchProgress>();
+            IBasicLogger logger = Substitute.For<IBasicLogger>();
+
+            patch.Apply(file, progress, logger);
+
+            Assert.Equal(6, file.configs.Count);
+
+            Assert.Same(urlConfig1, file.configs[0]);
+            AssertNodesEqual(new TestConfigNode("NODE")
+            {
+                { "foo", "bar" },
+            }, file.configs[0].config);
+
+            Assert.Same(urlConfig2, file.configs[1]);
+            AssertNodesEqual(new TestConfigNode("NODE")
+            {
+                { "foo", "bar" },
+            }, file.configs[1].config);
+
+            Assert.Same(urlConfig3, file.configs[2]);
+            AssertNodesEqual(new ConfigNode("NODE"), file.configs[2].config);
+
+            Assert.Same(urlConfig4, file.configs[3]);
+            AssertNodesEqual(new ConfigNode("NODE"), file.configs[3].config);
+            
+            AssertNodesEqual(new TestConfigNode("NODE")
+            {
+                { "foo", "baz" },
+                { "pqr", "stw" },
+            }, file.configs[4].config);
+            
+            AssertNodesEqual(new TestConfigNode("NODE")
+            {
+                { "pqr", "stw" },
+            }, file.configs[5].config);
+
+            Received.InOrder(delegate
+            {
+                progress.ApplyingCopy(urlConfig2, patch.UrlConfig);
+                progress.ApplyingCopy(urlConfig4, patch.UrlConfig);
+            });
+
+            progress.DidNotReceiveWithAnyArgs().ApplyingUpdate(null, null);
+            progress.DidNotReceiveWithAnyArgs().ApplyingDelete(null, null);
+
+            progress.DidNotReceiveWithAnyArgs().Error(null, null);
+            progress.DidNotReceiveWithAnyArgs().Exception(null, null);
+            progress.DidNotReceiveWithAnyArgs().Exception(null, null, null);
+        }
+
+        [Fact]
+        public void TestApply__NameChanged()
+        {
+            UrlDir.UrlFile file = UrlBuilder.CreateFile("abc/def.cfg");
+
+            UrlDir.UrlConfig urlConfig = UrlBuilder.CreateConfig(new TestConfigNode("NODE")
+            {
+                { "name", "000" },
+                { "foo", "bar" },
+            }, file);
+
+            INodeMatcher nodeMatcher = Substitute.For<INodeMatcher>();
+
+            nodeMatcher.IsMatch(urlConfig.config).Returns(true);
+
+            CopyPatch patch = new CopyPatch(UrlBuilder.CreateConfig("ghi/jkl", new TestConfigNode("@NODE")
+            {
+                { "@name", "001" },
+                { "@foo", "baz" },
+                { "pqr", "stw" },
+            }), nodeMatcher);
+
+            IPatchProgress progress = Substitute.For<IPatchProgress>();
+            IBasicLogger logger = Substitute.For<IBasicLogger>();
+
+            patch.Apply(file, progress, logger);
+
+            Assert.Equal(2, file.configs.Count);
+
+            Assert.Same(urlConfig, file.configs[0]);
+            AssertNodesEqual(new TestConfigNode("NODE")
+            {
+                { "name", "000" },
+                { "foo", "bar" },
+            }, file.configs[0].config);
+            
+            AssertNodesEqual(new TestConfigNode("NODE")
+            {
+                { "name", "001" },
+                { "foo", "baz" },
+                { "pqr", "stw" },
+            }, file.configs[1].config);
+            
+            progress.Received().ApplyingCopy(urlConfig, patch.UrlConfig);
+
+            progress.DidNotReceiveWithAnyArgs().ApplyingUpdate(null, null);
+            progress.DidNotReceiveWithAnyArgs().ApplyingDelete(null, null);
+
+            progress.DidNotReceiveWithAnyArgs().Error(null, null);
+            progress.DidNotReceiveWithAnyArgs().Exception(null, null);
+            progress.DidNotReceiveWithAnyArgs().Exception(null, null, null);
+        }
+
+        [Fact]
+        public void TestApply__NameNotChanged()
+        {
+            UrlDir.UrlFile file = UrlBuilder.CreateFile("abc/def.cfg");
+
+            UrlDir.UrlConfig urlConfig = UrlBuilder.CreateConfig(new TestConfigNode("NODE")
+            {
+                { "name", "000" },
+                { "foo", "bar" },
+            }, file);
+
+            INodeMatcher nodeMatcher = Substitute.For<INodeMatcher>();
+
+            nodeMatcher.IsMatch(urlConfig.config).Returns(true);
+
+            CopyPatch patch = new CopyPatch(UrlBuilder.CreateConfig("ghi/jkl", new TestConfigNode("+NODE")
+            {
+                { "@foo", "baz" },
+                { "pqr", "stw" },
+            }), nodeMatcher);
+
+            IPatchProgress progress = Substitute.For<IPatchProgress>();
+            IBasicLogger logger = Substitute.For<IBasicLogger>();
+
+            patch.Apply(file, progress, logger);
+
+            Assert.Equal(1, file.configs.Count);
+
+            Assert.Same(urlConfig, file.configs[0]);
+            AssertNodesEqual(new TestConfigNode("NODE")
+            {
+                { "name", "000" },
+                { "foo", "bar" },
+            }, file.configs[0].config);
+
+            progress.Received().Error(patch.UrlConfig, "Error - when applying copy ghi/jkl/+NODE to abc/def/NODE - the copy needs to have a different name than the parent (use @name = xxx)");
+
+            progress.DidNotReceiveWithAnyArgs().ApplyingUpdate(null, null);
+            progress.DidNotReceiveWithAnyArgs().ApplyingCopy(null, null);
+            progress.DidNotReceiveWithAnyArgs().ApplyingDelete(null, null);
+            
+            progress.DidNotReceiveWithAnyArgs().Exception(null, null);
+            progress.DidNotReceiveWithAnyArgs().Exception(null, null, null);
+        }
+
+        [Fact]
+        public void TestApply__FileNull()
+        {
+            CopyPatch patch = new CopyPatch(UrlBuilder.CreateConfig("abc/def", new ConfigNode()), Substitute.For<INodeMatcher>());
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                patch.Apply(null, Substitute.For<IPatchProgress>(), Substitute.For<IBasicLogger>());
+            });
+
+            Assert.Equal("file", ex.ParamName);
+        }
+
+        [Fact]
+        public void TestApply__ProgressNull()
+        {
+            CopyPatch patch = new CopyPatch(UrlBuilder.CreateConfig("abc/def", new ConfigNode()), Substitute.For<INodeMatcher>());
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                patch.Apply(UrlBuilder.CreateFile("abc/def.cfg"), null, Substitute.For<IBasicLogger>());
+            });
+
+            Assert.Equal("progress", ex.ParamName);
+        }
+
+        [Fact]
+        public void TestApply__LoggerNull()
+        {
+            CopyPatch patch = new CopyPatch(UrlBuilder.CreateConfig("abc/def", new ConfigNode()), Substitute.For<INodeMatcher>());
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                patch.Apply(UrlBuilder.CreateFile("abc/def.cfg"), Substitute.For<IPatchProgress>(), null);
+            });
+
+            Assert.Equal("logger", ex.ParamName);
+        }
+
+        private void AssertNodesEqual(ConfigNode expected, ConfigNode actual)
+        {
+            Assert.Equal(expected.ToString(), actual.ToString());
+        }
+    }
+}
