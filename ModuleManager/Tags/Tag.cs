7d994ad71842dd85e4c48efcdeed075d8ed09dd3
blowfish <joeydwong@gmail.com>
2018-07-03 23:53:23 -0700

    Tag, TagList, TagListParser
    A lot of things in MM are structured like :tag[value]trailer and this formalizes that structure

---
+++ ModuleManager/Tags/Tag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ModuleManager.Tags
+{
+    public struct Tag
+    {
+        public readonly string key;
+        public readonly string value;
+        public readonly string trailer;
+
+        public Tag(string key, string value, string trailer)
+        {
+            this.key = key ?? throw new ArgumentNullException(nameof(key));
+            if (key == string.Empty) throw new ArgumentException("can't be empty", nameof(key));
+
+            if (value == null && trailer != null)
+                throw new ArgumentException("trailer must be null if value is null");
+
+            if (trailer == string.Empty) throw new ArgumentException("can't be empty (null allowed)", nameof(trailer));
+
+            this.value = value;
+            this.trailer = trailer;
+        }
+
+        public override string ToString()
+        {
+            string s = "< '" + key + "' ";
+            if (value != null) s += "[ '" + value + "' ] ";
+            if (trailer != null) s += "'" + trailer + "' ";
+            s += ">";
+            return s;
+        }
+    }
+}
---
+++ ModuleManagerTests/Tags/TagListTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xunit;
+using ModuleManager.Tags;
+
+namespace ModuleManagerTests.Tags
+{
+    public class TagListTest
+    {
+        [Fact]
+        public void TestPrimaryTag()
+        {
+            Tag primaryTag = new Tag("stuff", null, null);
+            TagList tagList = new TagList(primaryTag, new Tag[0]);
+
+            Assert.Equal(primaryTag, tagList.PrimaryTag);
+        }
+
+        [Fact]
+        public void TestEnumeration()
+        {
+            Tag primaryTag = new Tag("stuff", null, null);
+            Tag tag1 = new Tag("tag1", null, null);
+            Tag tag2 = new Tag("tag2", null, null);
+
+            Tag[] tags = new Tag[] { tag1, tag2 };
+            TagList tagList = new TagList(primaryTag, tags);
+
+            tags[0] = new Tag("tag3", null, null);
+
+            Assert.Equal(new[] { tag1, tag2 }, tagList);
+        }
+
+        [Fact]
+        public void TestConstructor__TagsNull()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                new TagList(new Tag("blah", null, null), null);
+            });
+
+            Assert.Equal("tags", ex.ParamName);
+        }
+    }
+}
