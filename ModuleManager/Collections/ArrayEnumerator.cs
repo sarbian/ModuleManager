106581c492eaeee390a2c35b404a299acfcbea3e
blowfish <joeydwong@gmail.com>
2018-07-03 23:39:52 -0700

    Make ArrayEnumerator more versatile
    Now optionally accepts start index and length

--- ModuleManager/Collections/ArrayEnumerator.cs
+++ ModuleManager/Collections/ArrayEnumerator.cs
@@ -7,12 +7,37 @@ namespace ModuleManager.Collections
     public struct ArrayEnumerator<T> : IEnumerator<T>
     {
         private readonly T[] array;
+        private readonly int startIndex;
+        private readonly int length;
+
         private int index;
 
-        public ArrayEnumerator(params T[] array)
+        public ArrayEnumerator(params T[] array) : this(array, 0) { }
+
+        public ArrayEnumerator(T[] array, int startIndex) : this(array, startIndex, (array?.Length ?? -1) - startIndex) { }
+
+        public ArrayEnumerator(T[] array, int startIndex, int length)
         {
-            this.array = array;
-            index = -1;
+            this.array = array ?? throw new ArgumentNullException(nameof(array));
+
+            if (startIndex < 0)
+                throw new ArgumentException($"must be non-negative (got {startIndex})", nameof(startIndex));
+            if (startIndex > array.Length)
+                throw new ArgumentException(
+                    $"must be less than or equal to array length (array length {array.Length}, startIndex {startIndex})",
+                    nameof(startIndex)
+                );
+            if (length < 0)
+                throw new ArgumentException($"must be non-negative (got {length})", nameof(length));
+            if (startIndex + length > array.Length)
+                throw new ArgumentException(
+                    $"must fit within the string (array length {array.Length}, startIndex {startIndex}, length {length})",
+                    nameof(length)
+                );
+
+            this.startIndex = startIndex;
+            this.length = length;
+            index = startIndex - 1;
         }
 
         public T Current => array[index];
@@ -23,12 +48,12 @@ namespace ModuleManager.Collections
         public bool MoveNext()
         {
             index++;
-            return index < array.Length;
+            return index < startIndex + length;
         }
 
         public void Reset()
         {
-            index = -1;
+            index = startIndex - 1;
         }
     }
 }
