99984913ac564b5a06860a4094bbafe4ff335450
blowfish <joeydwong@gmail.com>
2018-07-03 23:39:52 -0700

    Make ArrayEnumerator more versatile
    Now optionally accepts start index and length

--- ModuleManagerTests/Collections/ArrayEnumeratorTest.cs
+++ ModuleManagerTests/Collections/ArrayEnumeratorTest.cs
@@ -8,6 +8,69 @@ namespace ModuleManagerTests.Collections
     public class ArrayEnumeratorTest
     {
         [Fact]
+        public void Test__Constructor__ArrayNull()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                new ArrayEnumerator<string>(null);
+            });
+
+            Assert.Equal("array", ex.ParamName);
+        }
+
+        [Fact]
+        public void Test__Constructor__StartIndex__Negative()
+        {
+            string[] arr = { "abc", "def", "ghi" };
+            ArgumentException ex = Assert.Throws<ArgumentException>(delegate
+            {
+                new ArrayEnumerator<string>(arr, -1);
+            });
+
+            Assert.Equal("startIndex", ex.ParamName);
+            Assert.Contains("must be non-negative (got -1)", ex.Message);
+        }
+
+        [Fact]
+        public void Test__Constructor__StartIndex__TooLarge()
+        {
+            string[] arr = { "abc", "def", "ghi" };
+            ArgumentException ex = Assert.Throws<ArgumentException>(delegate
+            {
+                new ArrayEnumerator<string>(arr, 4);
+            });
+
+            Assert.Equal("startIndex", ex.ParamName);
+            Assert.Contains("must be less than or equal to array length (array length 3, startIndex 4)", ex.Message);
+        }
+
+        [Fact]
+        public void Test__Constructor__Length__Negative()
+        {
+            string[] arr = { "abc", "def", "ghi" };
+            ArgumentException ex = Assert.Throws<ArgumentException>(delegate
+            {
+                new ArrayEnumerator<string>(arr, 1, -1);
+            });
+
+            Assert.Equal("length", ex.ParamName);
+            Assert.Contains("must be non-negative (got -1)", ex.Message);
+        }
+
+        [Fact]
+        public void Test__Constructor__Length__TooLong()
+        {
+            string[] arr = { "abc", "def", "ghi" };
+            ArgumentException ex = Assert.Throws<ArgumentException>(delegate
+            {
+                new ArrayEnumerator<string>(arr, 1, 3);
+            });
+
+            Assert.Equal("length", ex.ParamName);
+            Assert.Contains("must fit within the string (array length 3, startIndex 1, length 3)", ex.Message);
+        }
+
+        [Fact]
         public void TestArrayEnumerator()
         {
             string[] arr = { "abc", "def", "ghi" };
@@ -50,6 +113,34 @@ namespace ModuleManagerTests.Collections
         {
             string[] arr = new string[0];
             IEnumerator<string> enumerator = new ArrayEnumerator<string>(arr);
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public void TestArrayEnumerator__StartIndex()
+        {
+            string[] arr = { "abc", "def", "ghi" };
+
+            IEnumerator<string> enumerator = new ArrayEnumerator<string>(arr, 1);
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal("def", enumerator.Current);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal("ghi", enumerator.Current);
+            Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public void TestArrayEnumerator__StartIndex__Length()
+        {
+            string[] arr = { "abc", "def", "ghi", "jkl" };
+
+            IEnumerator<string> enumerator = new ArrayEnumerator<string>(arr, 1, 2);
+
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal("def", enumerator.Current);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal("ghi", enumerator.Current);
             Assert.False(enumerator.MoveNext());
         }
     }
