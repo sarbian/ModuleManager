ea7b05a6358295f43f20d02aba4095e76e58fef3
blowfish <joeydwong@gmail.com>
2018-10-30 22:48:55 -0700

    Extract paths to their own class
    Forgot that the static initalizers would prevent tests from running

---
+++ ModuleManager/FilePathRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ModuleManager
+{
+    internal static class FilePathRepository
+    {
+        internal static readonly string cachePath = Path.Combine(Path.Combine(KSPUtil.ApplicationRootPath, "GameData"), "ModuleManager.ConfigCache");
+
+        internal static readonly string techTreeFile = Path.Combine("GameData", "ModuleManager.TechTree");
+        internal static readonly string techTreePath = Path.Combine(KSPUtil.ApplicationRootPath, techTreeFile);
+
+        internal static readonly string physicsFile = Path.Combine("GameData", "ModuleManager.Physics");
+        internal static readonly string physicsPath = Path.Combine(KSPUtil.ApplicationRootPath, physicsFile);
+        internal static readonly string defaultPhysicsPath = Path.Combine(KSPUtil.ApplicationRootPath, "Physics.cfg");
+
+        internal static readonly string partDatabasePath = Path.Combine(KSPUtil.ApplicationRootPath, "PartDatabase.cfg");
+
+        internal static readonly string shaPath = Path.Combine(Path.Combine(KSPUtil.ApplicationRootPath, "GameData"), "ModuleManager.ConfigSHA");
+    }
+}
--- ModuleManager/MMPatchLoader.cs
+++ ModuleManager/MMPatchLoader.cs
@@ -21,6 +21,8 @@ using ModuleManager.Patches;
 using ModuleManager.Progress;
 using NodeStack = ModuleManager.Collections.ImmutableStack<ConfigNode>;
 
+using static ModuleManager.FilePathRepository;
+
 namespace ModuleManager
 {
     [SuppressMessage("ReSharper", "StringLastIndexOfIsCultureSpecific.1")]
@@ -36,19 +38,6 @@ namespace ModuleManager
         private string activity = "Module Manager";
 
         private static readonly Dictionary<string, Regex> regexCache = new Dictionary<string, Regex>();
-
-        private static readonly string cachePath = Path.Combine(Path.Combine(KSPUtil.ApplicationRootPath, "GameData"), "ModuleManager.ConfigCache");
-
-        internal static readonly string techTreeFile = Path.Combine("GameData", "ModuleManager.TechTree");
-        internal static readonly string techTreePath = Path.Combine(KSPUtil.ApplicationRootPath, techTreeFile);
-
-        internal static readonly string physicsFile = Path.Combine("GameData", "ModuleManager.Physics");
-        internal static readonly string physicsPath = Path.Combine(KSPUtil.ApplicationRootPath, physicsFile);
-        private static readonly string defaultPhysicsPath = Path.Combine(KSPUtil.ApplicationRootPath, "Physics.cfg");
-
-        internal static readonly string partDatabasePath = Path.Combine(KSPUtil.ApplicationRootPath, "PartDatabase.cfg");
-
-        private static readonly string shaPath = Path.Combine(Path.Combine(KSPUtil.ApplicationRootPath, "GameData"), "ModuleManager.ConfigSHA");
 
         private UrlDir.UrlFile physicsUrlFile;
 
