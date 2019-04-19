a0c1dfc2c7342c8ee1010bee9a3019a3d4eb2b31
blowfishpro <joeydwong@gmail.com>
2019-04-19 16:33:05 -0700

    add test for ProtoUrlConfig

---
+++ ModuleManagerTests/ProtoUrlConfigTest.cs
@@ -0,0 +1,74 @@
+using System;
+using Xunit;
+using TestUtils;
+using ModuleManager;
+
+namespace ModuleManagerTests
+{
+    public class ProtoUrlConfigTest
+    {
+        [Fact]
+        public void TestContructor__UrlFileNull()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                new ProtoUrlConfig(null, new ConfigNode());
+            });
+
+            Assert.Equal("urlFile", ex.ParamName);
+        }
+
+        [Fact]
+        public void TestContructor__NodeNull()
+        {
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(delegate
+            {
+                new ProtoUrlConfig(UrlBuilder.CreateFile("foo/bar"), null);
+            });
+
+            Assert.Equal("node", ex.ParamName);
+        }
+
+        [Fact]
+        public void TestUrlFile()
+        {
+            UrlDir.UrlFile urlFile = UrlBuilder.CreateFile("abc/def.cfg");
+            ProtoUrlConfig protoUrlConfig = new ProtoUrlConfig(urlFile, new ConfigNode());
+
+            Assert.Same(urlFile, protoUrlConfig.UrlFile);
+        }
+
+        [Fact]
+        public void TestNode()
+        {
+            ConfigNode node = new ConfigNode("NODE");
+            ProtoUrlConfig protoUrlConfig = new ProtoUrlConfig(UrlBuilder.CreateFile("foo/bar"), node);
+
+            Assert.Same(node, protoUrlConfig.Node);
+        }
+
+        [Fact]
+        public void TestFileUrl()
+        {
+            ProtoUrlConfig protoUrlConfig = new ProtoUrlConfig(UrlBuilder.CreateFile("abc/def.cfg"), new ConfigNode());
+
+            Assert.Equal("abc/def.cfg", protoUrlConfig.FileUrl);
+        }
+
+        [Fact]
+        public void TestNodeType()
+        {
+            ProtoUrlConfig protoUrlConfig = new ProtoUrlConfig(UrlBuilder.CreateFile("abc/def"), new ConfigNode("SOME_NODE"));
+
+            Assert.Equal("SOME_NODE", protoUrlConfig.NodeType);
+        }
+
+        [Fact]
+        public void TestFullUrl()
+        {
+            ProtoUrlConfig protoUrlConfig = new ProtoUrlConfig(UrlBuilder.CreateFile("abc/def.cfg"), new ConfigNode("SOME_NODE"));
+
+            Assert.Equal("abc/def.cfg/SOME_NODE", protoUrlConfig.FullUrl);
+        }
+    }
+}
