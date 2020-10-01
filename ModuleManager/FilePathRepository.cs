0261dc6dac6b3340357a02c8322d21770be9c09c
blowfishpro <blowfishpro@users.noreply.github.com>
2020-09-30 22:15:15 -0700

    Normalize KSP root path
    KSP makes it weird
    
    Addresses confusion in #164

--- ModuleManager/FilePathRepository.cs
+++ ModuleManager/FilePathRepository.cs
@@ -5,20 +5,21 @@ namespace ModuleManager
 {
     internal static class FilePathRepository
     {
-        internal static readonly string cachePath = Path.Combine(KSPUtil.ApplicationRootPath, "GameData", "ModuleManager.ConfigCache");
+        internal static readonly string normalizedRootPath = Path.GetFullPath(KSPUtil.ApplicationRootPath);
+        internal static readonly string cachePath = Path.Combine(normalizedRootPath, "GameData", "ModuleManager.ConfigCache");
 
         internal static readonly string techTreeFile = Path.Combine("GameData", "ModuleManager.TechTree");
-        internal static readonly string techTreePath = Path.Combine(KSPUtil.ApplicationRootPath, techTreeFile);
+        internal static readonly string techTreePath = Path.Combine(normalizedRootPath, techTreeFile);
 
         internal static readonly string physicsFile = Path.Combine("GameData", "ModuleManager.Physics");
-        internal static readonly string physicsPath = Path.Combine(KSPUtil.ApplicationRootPath, physicsFile);
-        internal static readonly string defaultPhysicsPath = Path.Combine(KSPUtil.ApplicationRootPath, "Physics.cfg");
+        internal static readonly string physicsPath = Path.Combine(normalizedRootPath, physicsFile);
+        internal static readonly string defaultPhysicsPath = Path.Combine(normalizedRootPath, "Physics.cfg");
 
-        internal static readonly string partDatabasePath = Path.Combine(KSPUtil.ApplicationRootPath, "PartDatabase.cfg");
+        internal static readonly string partDatabasePath = Path.Combine(normalizedRootPath, "PartDatabase.cfg");
 
-        internal static readonly string shaPath = Path.Combine(KSPUtil.ApplicationRootPath, "GameData", "ModuleManager.ConfigSHA");
+        internal static readonly string shaPath = Path.Combine(normalizedRootPath, "GameData", "ModuleManager.ConfigSHA");
 
-        internal static readonly string logsDirPath = Path.Combine(KSPUtil.ApplicationRootPath, "Logs", "ModuleManager");
+        internal static readonly string logsDirPath = Path.Combine(normalizedRootPath, "Logs", "ModuleManager");
         internal static readonly string logPath = Path.Combine(logsDirPath, "ModuleManager.log");
         internal static readonly string patchLogPath = Path.Combine(logsDirPath, "MMPatch.log");
     }
